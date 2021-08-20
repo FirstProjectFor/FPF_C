c4a092e72a78fd19c096b2d1d4f69f9c4579d3ef
sunfeilong <sunfeilong1993@gmail.com>
2021-08-20 15:35:15 +0800

    c struct

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
-#include <float.h>
+#include "header.h"
 
+struct Person {
+    char name[50];
+    char address[200];
+    int age;
+};
+
+int testStruct() {
+
+    struct Person p1;
+    strcpy(p1.name, "zs");
+    strcpy(p1.address, "bj");
+    p1.age = 20;
+
+    printf("%s\n", p1.name);
+    printf("%s\n", p1.address);
+    printf("%d\n", p1.age);
+    return 0;
+}
+
+int add(int x, int y) {
+    return x + y;
+};
 
 int main(int argc, char *args[]) {
+
     printf("Hello World!\n");
     printf("%d\n", argc);
     printf("%s\n", args[0]);
@@ -13,5 +35,19 @@ int main(int argc, char *args[]) {
     printf("%llu\n", sizeof(double));
     printf("%f\n", DBL_MIN);
     printf("%f\n", DBL_MAX);
-    return 0;
+
+    int a = 0;
+    printf("%p\n", &a);
+
+    //func_ptr
+    int (*addFunc)(int, int) = &add;
+    int sum = addFunc(1, 2);
+    printf("%d\n", sum);
+
+    char name[] = "ZS";
+    printf("%s\n", name);
+    printf("%llu\n", strlen(name));
+
+    //run struct
+    testStruct();
 }
