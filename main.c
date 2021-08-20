0fe2abf7bcec167636ff82b40dcbea97cff54120
sunfeilong <sunfeilong1993@gmail.com>
2021-08-20 12:56:40 +0800

    c

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
-//
-// Created by sunfeilong on 2021/8/20.
-//
-
 #include <stdio.h>
+#include <float.h>
 
-int main() {
+
+int main(int argc, char *args[]) {
     printf("Hello World!\n");
+    printf("%d\n", argc);
+    printf("%s\n", args[0]);
+    printf("%llu\n", sizeof(int));
+    printf("%llu\n", sizeof(float));
+    printf("%f\n", FLT_MIN);
+    printf("%f\n", FLT_MAX);
+    printf("%llu\n", sizeof(double));
+    printf("%f\n", DBL_MIN);
+    printf("%f\n", DBL_MAX);
     return 0;
 }
